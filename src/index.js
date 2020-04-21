a51664a020f1502f501aeb8eed22aa568b13d740
Pedro Borges <ppmbjr@gmail.com>
2020-04-21 13:19:50 -0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import {StatusBar} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import '~/config/ReactotronConfig';
@@ -8,6 +9,7 @@ import Router from '~/routes/index';
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
       <Router />
     </NavigationContainer>
   );
